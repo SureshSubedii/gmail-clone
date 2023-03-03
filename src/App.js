7e0e8a0ba58cde43c0244a5213823702cbfc873d
SureshSubedii <sureshsubedi485@gmail.com>
2023-03-03 15:29:32 +0545

    Work In progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import './App.css';
+import EmailList from './EmailList';
 import Header from './Header';
+import Mail from './Mail';
 import Sidebar from './Sidebar';
 
 function App() {
   document.body.style.backgroundColor='#ffffff'
   return (
+    <Router>
     <div className="app">
     <Header/>
+    <div className="app_body">
     <Sidebar/>
+    <Routes>
+    <Route exact path="/" element={<EmailList/>}/>
+    <Route exact  path="/mail" element={<Mail/>}/>
+
+    </Routes>
+
+
     </div>
+    </div>
+    </Router>
   );
 }
 
---
+++ src/Mail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Mail.css'
+
+function Mail() {
+  return (
+    <div><h1>MAil Here</h1> </div>
+  )
+}
+
+export default Mail
