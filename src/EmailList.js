ece96a7b6239e7f3a1252eaaa7d1ec3289ae997c
SureshSubedii <sureshsubedi485@gmail.com>
2023-03-05 11:30:48 +0545

    lll

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,19 +1,32 @@
-import { ArrowDropDown, CheckBox, ChevronLeft, ChevronRight, KeyboardHide, MoreVert, Refresh } from '@mui/icons-material'
+import { ArrowDropDown,ChevronLeft, ChevronRight, KeyboardHide, MoreVert, Refresh } from '@mui/icons-material'
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import { IconButton } from '@mui/material'
 import React from 'react'
 import './EmailList.css'
 
 function EmailList() {
+  // const gridStyle = {
+  //   display: 'grid',
+  //   gridGap: '16px',
+  //   gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+  // };
+  // if (window.innerWidth < 413) {
+  //   gridStyle.gridTemplateColumns = 'repeat(auto-fit, minmax(150px, 1fr))';
+  //   gridStyle.gridGap = '8px';
+  // }
   return (
     <div className='emailList'>
+
       <div className="email_settings">
         <div className="emailList_settingsLeft">
+        <IconButton>
         <CheckBoxOutlineBlankIcon/>
-
+        </IconButton>
         <IconButton>
           <ArrowDropDown/>
           </IconButton>
+
+          
         <IconButton>
           <Refresh/>
         </IconButton>
@@ -44,9 +57,9 @@ function EmailList() {
         </div>
 
       </div>
-      <div className="emailList_sections">
+      {/* <div className="emailList_sections">
 
-      </div>
+      </div> */}
 
     </div>
   )
